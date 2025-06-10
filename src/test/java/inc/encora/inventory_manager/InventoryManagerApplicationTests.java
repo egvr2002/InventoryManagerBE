1b68f212275ef8a7707eb636a342c598bce1187f
Emmanuel Valentin <emmanuel.valentin@encora.com>
2025-06-10 10:00:08 -0600

    (closes #1) Spring Boot project setup

---
+++ src/test/java/inc/encora/inventory_manager/InventoryManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package inc.encora.inventory_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
